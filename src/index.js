41462ece05adfa702e6611ef16cfecb6b1ccb916
Your Name <your.email@example.com>
2025-12-28 16:08:51 +0300

    Чтение и парсинг файлов

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Command } from 'commander'
+import { getDataFromFile } from './parsers.js'
 
 const program = new Command()
 
@@ -9,5 +10,13 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const data1 = getDataFromFile(filepath1)
+    const data2 = getDataFromFile(filepath2)
+
+    console.log('Successfully parsed files:')
+    console.log(`${filepath1}:`, data1)
+    console.log(`${filepath2}:`, data2)
+  })
 
 program.parse()
